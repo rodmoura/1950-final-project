a94b326631ac243984b96e58acf87a556c13168c
Rodrigo Moura <rodmoura82@gmail.com>
2016-12-06 23:34:16 -0800

    Fixed issue on sticky nav bar

--- final/assets/js/sticky-navbar.js
+++ final/assets/js/sticky-navbar.js
@@ -9,11 +9,13 @@ $(document).ready(function (){
         // detect if the user is at the top of the page...
 		if($(window).scrollTop() <= $heightOfHeader) {			
             $("nav").css("position","static");
+            $("main").css("top","0px");
 		} else {
 		// ...or if the user has scrolled away from the top
 			$("nav").css("position","fixed");
             $("nav").css("z-index","10000");
 			$("nav").css("top","0px");
+            $("main").css("top","57px");
 		}
 	});
 });
