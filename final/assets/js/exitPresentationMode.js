f1db13eaec02f97a059c1c97b870a34cead2ab41
Paulo Leao <paulo.leao@live.com>
2016-12-06 23:19:55 -0800

    large screen style and lecture presentation exit

---
+++ final/assets/js/exitPresentationMode.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".lecture-exit").click(function(){
+        $("header").css("display","block");
+        $("footer").css("display","block");
+        $(".nav-menulist").css("display","inline-block");
+        $(".lecture-exit").css("visibility","hidden");
+    });
+});
